00fad521e07de6cd9a965bbe920ca160193828c5
vishwa-radhya <vishuvishwa735@gmail.com>
2024-05-27 19:31:13 +0530

    changed to dry code

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
 const toggleSwitch=document.querySelector('input[type="checkbox"]');
 const nav=document.getElementById('nav');
 const toggleIcon=document.getElementById('toggle-icon');
-const image1=document.getElementById('image1');
-const image2=document.getElementById('image2');
-const image3=document.getElementById('image3');
-const textBox=document.getElementById('text-box');
 
-
-function darkmode(){
-    nav.style.backgroundColor='rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor='rgb(255 255 255 / 50%)';
-    // console.log(toggleIcon.children);
-    toggleIcon.children[0].textContent='Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun','fa-moon');
-    imageMode('d')
+function toggleDarkLightMode(isDark){
+    nav.style.backgroundColor= isDark? 'rgb(0 0 0 / 50%)' :'rgb(0 0 0 / 50%)';
+    document.getElementById('text-box').style.backgroundColor= isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent=isDark ? 'Dark Mode' : 'Light Mode';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun','fa-moon') : toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
+    isDark ? imageMode('d') : imageMode('l');
 }
 
 function imageMode(color){
-    image1.src=`img/notebook_${color}.svg`;
-    image2.src=`img/progress_${color}.svg`;
-    image3.src=`img/rain_${color}.svg`;
+    const images=['notebook','progress','rain'];
+    images.forEach((image,index)=>{
+        document.getElementById(`image${index+1}`).src=`img/${image}_${color}.svg`;
+    })
 }
 
-function lightmode(){
-    nav.style.backgroundColor='rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor='rgb(0 0 0 / 50%)';
-    // console.log(toggleIcon.children);
-    toggleIcon.children[0].textContent='Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
-    imageMode('l');
+function switchThemeHandler(theme,bool){
+    document.documentElement.setAttribute('data-theme',theme);
+    localStorage.setItem('theme',theme);
+    toggleDarkLightMode(bool);
 }
-
 
 function switchTheme(event){
     // console.log(event.target.checked);
-    if(event.target.checked){
-        document.documentElement.setAttribute('data-theme','dark');
-        darkmode();
-    }else{
-        document.documentElement.setAttribute('data-theme','light')
-        lightmode();
-    }
+    event.target.checked ? switchThemeHandler('dark',true) : switchThemeHandler('light',false);
 }
 
 toggleSwitch.addEventListener('change',switchTheme);
+
+const currentTheme = localStorage.getItem('theme')
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme',currentTheme);
+    if(currentTheme==='dark'){
+        toggleSwitch.checked=true;
+        toggleDarkLightMode(true);
+    }
+}
